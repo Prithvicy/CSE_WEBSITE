ba12969d5fc9a3f7c2461201a24fb97f107fee8d
Prith Kami <prithvi40201@gmail.com>
2022-05-14 18:14:16 +0530

    header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,205 +1,66 @@
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  makeStyles,
-  Button,
-  IconButton,
-  Drawer,
-  Link,
-  MenuItem,
-} from "@material-ui/core";
-import MenuIcon from "@material-ui/icons/Menu";
-import React, { useState, useEffect } from "react";
-import { Link as RouterLink } from "react-router-dom";
+import React from "react";
+import "./Header.css";
+import logo from "../Assets/logoDummy.png";
+import { Link } from "react-router-dom";
 
-const headersData = [
-  {
-    h1: "Home",
-    href: "/home",
-  },
-  {
-    h1: "Faculty",
-    href: "/faculty",
-  },
-  {
-    h1: "Research",
-    href: "/research",
-  },
-  {
-    h1: "Students",
-    href: "/students",
-  },
-  {
-    h1: "Career",
-    href: "/career",
-  },
- 
-  {
-    h1: "Student Chapter",
-    href: "/studentChapter",
-  },
-  {
-    h1: "Testemonials",
-    href: "/testemonials",
-  },
-];
-
-const useStyles = makeStyles(() => ({
-  header: {
-    backgroundColor: "transparent",
-    paddingRight: "79px",
-    paddingLeft: "118px",
-    
-    "@media (max-width: 900px)": {
-      paddingLeft: 0,
-      marginTop:0,
-      
-    },
-  },
-  logo: {
-    fontFamily: "Work Sans, sans-serif",
-    fontWeight: 600,
-    color: "#FFFEFE",
-    textAlign: "left",
-  },
-  menuButton: {
-    fontFamily: "Open Sans, sans-serif",
-    fontWeight: 700,
-    size: "18px",
-    marginLeft: "38px",
-    textDecoration:"none",
-  },
-  toolbar: {
-    display: "flex",
-    justifyContent: "space-between",
-  },
-  drawerContainer: {
-    padding: "20px 30px",
-  },
-  
-}));
-
-export default function Header() {
-  const { header, logo, menuButton, toolbar, drawerContainer } = useStyles();
-
-  const [state, setState] = useState({
-    mobileView: false,
-    drawerOpen: false,
-  });
-
-  const { mobileView, drawerOpen } = state;
-
-  useEffect(() => {
-    const setResponsiveness = () => {
-      return window.innerWidth < 900
-        ? setState((prevState) => ({ ...prevState, mobileView: true }))
-        : setState((prevState) => ({ ...prevState, mobileView: false }));
-    };
-
-    setResponsiveness();
-
-    window.addEventListener("resize", () => setResponsiveness());
-
-    return () => {
-      window.removeEventListener("resize", () => setResponsiveness());
-    };
-  }, []);
-
-  const displayDesktop = () => {
-    return (
-      <Toolbar className={toolbar}>
-        {CSELOGO}
-        <div>{getMenuButtons()}</div>
-      </Toolbar>
-    );
-  };
-
-  const displayMobile = () => {
-    const handleDrawerOpen = () =>
-      setState((prevState) => ({ ...prevState, drawerOpen: true }));
-    const handleDrawerClose = () =>
-      setState((prevState) => ({ ...prevState, drawerOpen: false }));
-
-    return (
-      <Toolbar>
-        <IconButton
-          {...{
-            edge: "start",
-            color: "inherit",
-            "aria-label": "menu",
-            "aria-haspopup": "true",
-            onClick: handleDrawerOpen,
-          }}
-        >
-          <MenuIcon />
-        </IconButton>
-
-        <Drawer
-          {...{
-            anchor: "left",
-            open: drawerOpen,
-            onClose: handleDrawerClose,
-          }}
-        >
-          <div className={drawerContainer}>{getDrawerChoices()}</div>
-        </Drawer>
-
-        <div>{CSELOGO}</div>
-      </Toolbar>
-    );
-  };
-
-  const getDrawerChoices = () => {
-    return headersData.map(({ h1, href }) => {
-      return (
-        <Link
-          {...{
-            component: RouterLink,
-            to: href,
-            color: "inherit",
-            style: { textDecoration: "none" },
-            key: h1,
-          }}
-        >
-                  <Button>
-
-          <MenuItem>{h1}</MenuItem>
-          </Button>
-        </Link>
-      );
-    });
-  };
-
-  const CSELOGO = (
-    <Typography variant="h6" component="h1" className={logo}>
-      CSE DEPARTMENT
-    </Typography>
-  );
-
-  const getMenuButtons = () => {
-    return headersData.map(({ h1, href }) => {
-      return (
-        <Link
-          {...{
-            key: h1,
-            color: "inherit",
-            to: href,
-            component: RouterLink,
-            className: menuButton,
-          }}
-        >
-          {h1}
-        </Link>
-      );
-    });
-  };
-
+const Header = () => {
   return (
-    <header>
-      <AppBar className={header}>
-        {mobileView ? displayMobile() : displayDesktop()}
-      </AppBar>
-    </header>
+    <div className="header">
+      <div className="header1" id="myHeader">
+        <Link to="/" className="logo">
+          <img src={logo} alt="" />
+        </Link>
+        <div className="header__items">
+      
+        <Link to="/regulations">
+          <span>Curriculum Regulations</span>
+        </Link>
+        <Link to="/faculty">
+          <span className="headbut">Faculty</span>
+        </Link>
+        <Link to="/research">
+          <span>Research</span>
+        </Link>
+        <Link to="/infrastructure">
+          <span>Infrastructure</span>
+        </Link>
+        <Link to="/testimonial">
+          <span>Testimonials</span>
+        </Link>
+        <Link to="/p">
+          <span>PEO and PSO</span>
+        </Link>
+        </div>
+
+      </div>
+      <div className="header2">
+        <Link to="/placemenets">
+          <span>Placements</span>
+        </Link>
+        <Link to="/hstudies">
+          <span>Higher Studies</span>
+        </Link>
+        <Link to="/rholders">
+          <span>Rank Holders </span>
+        </Link>
+        <Link to="/students">
+          <span>Students </span>
+        </Link>
+        <Link to="/ealumni">
+          <span>Elite Alumni </span>
+        </Link>
+        <Link to="/chapter">
+          <span>Chapter</span>
+        </Link>
+        <Link to="/club">
+          <span>Club </span>
+        </Link>
+        <Link to="/bmembers">
+          <span>bos members</span>
+        </Link>
+      </div>
+    </div>
   );
-}
+};
+
+export default Header;
